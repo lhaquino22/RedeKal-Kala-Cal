ea06076409e4279548a5a4133d5854d484cdc38e
felipebarroscaminha <felipebarroscaminha@gmail.com>
2019-06-13 01:09:16 -0300

    Initial commit

---
+++ src/Navigator.js
@@ -0,0 +1,13 @@
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+import HomeScreen from './HomeScreen';
+import ProfileScreen from './ProfileScreen';
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
